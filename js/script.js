93a12f3e69b7cb99d71ea396e578801e8cba4567
neVictor <kuzvictor36@gmail.com>
2022-01-29 18:08:06 +0200

    Some fixes
    Solve some problems

--- js/script.js
+++ js/script.js
@@ -1,2 +1,2 @@
-"use strict";var header_burger_btn=document.getElementById("header-menu-burger-btn"),header_menu_list=document.getElementById("header-menu-list");header_burger_btn.addEventListener("click",function(){header_menu_list.classList.toggle("menu__list_open")});
+"use strict";var header_burger_btn=document.getElementById("header-menu-burger-btn"),header_menu_list=document.getElementById("header-menu-list");header_burger_btn.addEventListener("click",function(){header_menu_list.classList.toggle("menu__list_open"),header_burger_btn.querySelector(".menu__burger-icon").classList.toggle("menu__burger-icon_close")});
 //# sourceMappingURL=../maps/script.js.map
